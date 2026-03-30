0fb249847e3656dc6654c7af161cd605823e96f7
Anitah-nitty <anitahreh856@gmail.com>
2026-03-30 13:21:49 +0300

    initial commit

---
+++ app/src/main/java/com/anitah/sokohub/ui/screens/contact/ContactScreen.kt
@@ -0,0 +1,15 @@
+package com.anitah.sokohub.ui.screens.contact
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ContactScreen(){
+
+}
+
+@Preview
+@Composable
+fun ContactScreenPreview(){
+    ContactScreen()
+}
---
+++ app/src/main/java/com/anitah/sokohub/ui/screens/service/ServiceScreen.kt
@@ -0,0 +1,15 @@
+package com.anitah.sokohub.ui.screens.service
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ServiceScreen(){
+
+}
+
+@Preview
+@Composable
+fun ServiceScreenPreview(){
+    ServiceScreen()
+}
